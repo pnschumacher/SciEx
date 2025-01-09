0dc1d75d266f1b4894e10ce4fa6befd3250ed4d8
pnschumacher <philipp.schumacher@gmx.de>
2025-01-09 13:38:15 +0100

    Update llm_solve_exam_with_cm.sh

--- llm_solve_exam_with_cm.sh
+++ llm_solve_exam_with_cm.sh
@@ -3,37 +3,15 @@ set -eu  # Crash if variable used without being set
 
 source venv/bin/activate
 source env_vars.sh
-export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH # this is done so python uses correct sqlite3 version
+export LD_LIBRARY_PATH=$HOME/.local/lib:${LD_LIBRARY_PATH:-}
 
-SERVER_TYPE=$1
-LLM_NAME=$2
-LLM_NAME_FULL=$3
-SERVER_URL=$4
-COURSE_MATERIAL_PATH=$5
-EMBEDDING_MODEL=$6
-SIMILARITY_TOP_K=$7
-
-if [ -z ${SERVER_TYPE} ]; then
-  SERVER_TYPE="openai"
-fi
-if [ -z ${LLM_NAME} ]; then
-  LLM_NAME="llama3.3"
-fi
-if [ -z ${LLM_NAME_FULL} ]; then
-  LLM_NAME_FULL="meta-llama/Llama-3.3-70B-Instruct"
-fi
-if [ -z ${SERVER_URL} ]; then
-  SERVER_URL="http://127.0.0.1:8080"  # Local llama.cpp, needs to be deployed first on same node
-fi
-if [ -z ${COURSE_MATERIAL_PATH} ]; then
-  COURSE_MATERIAL_PATH="$DEF_COURSE_MATERIAL_PATH" # defined in env_vars.sh
-fi
-if [ -z ${EMBEDDING_MODEL} ]; then
-  EMBEDDING_MODEL="BAAI/bge-large-en"
-fi
-if [ -z ${SIMILARITY_TOP_K} ]; then
-  SIMILARITY_TOP_K="10"
-fi
+SERVER_TYPE=${1:-"openai"}
+LLM_NAME=${2:-"llama3.3"}
+LLM_NAME_FULL=${3:-"meta-llama/Llama-3.3-70B-Instruct"}
+SERVER_URL=${4:-"http://127.0.0.1:8080"}  # Local llama.cpp, needs to be deployed first on same node
+COURSE_MATERIAL_PATH=${5:-"$DEF_COURSE_MATERIAL_PATH"}  # defined in env_vars.sh
+EMBEDDING_MODEL=${6:-"BAAI/bge-large-en"}
+SIMILARITY_TOP_K=${7:-"10"}
 
 # Loop through each JSON file in the current directory and its subdirectories
 for file in $(find exams_json/ -type f -name '*.json'); do
