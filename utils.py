b32ce4484a33dcc1a495cffb308cb8f61861ec6a
Philipp Schumacher <58042474+pnschumacher@users.noreply.github.com>
2025-01-10 15:32:30 +0100

    Use slide windows as documents

--- utils.py
+++ utils.py
@@ -406,42 +406,36 @@ def get_index(exam_json_path, embedding_model_name, course_material_path):
 
     if os.path.exists(slide_directory):
         slide_documents = SimpleDirectoryReader(slide_directory).load_data()
-        
+
         # TODO: Do not hardcode this on NLP exams
         slide_prefix_pattern = r'^[^\n]*Niehues[^\n]*\n'
         date_slide_pattern_en = r"\n(January|February|March|April|May|June|July|August|September|October|November|December) \d{1,2}, \d{4}\d{1,3}"
         date_slide_pattern_de = r"\n\d{1,2}. (Januar|Februar|März|April|Mai|Juni|Juli|August|September|Oktober|November|Dezember) \d{4}\d{1,3}"
         latex_pattern = r"<latexit.*?>.*?<\/latexit>"
 
-        pdf_documents = []
-        pdf_text = ""
-        previous_file_name = slide_documents[0].metadata["file_name"]
-        last_doc = None
-
         for doc in slide_documents:
             new_text = re.sub(slide_prefix_pattern, "", doc.text)
             new_text = re.sub(date_slide_pattern_en, "", new_text)
             new_text = re.sub(date_slide_pattern_de, "", new_text)
             new_text = re.sub(latex_pattern, "", new_text)
+
+            doc.text_resource.text = new_text
+
+        slide_window_documents = []
+        window_size = 5
+
+        for i in range(len(slide_window_documents) - window_size - 1):
+            document_text = ""
+            for j in range(window_size):
+                document_text += slide_documents[i + j].text
             
-            file_name = doc.metadata["file_name"]
-
-            if previous_file_name == file_name:
-                pdf_text += f"{new_text}\n\n"
-            else:
-                last_doc.text_resource.text = pdf_text
-                pdf_documents.append(last_doc)
-                pdf_text = new_text
-
-            previous_file_name = file_name
-            last_doc = doc 
-        
-        last_doc.text_resource.text = pdf_text
-        pdf_documents.append(last_doc)
+            slide_window_document = slide_documents[i].model_copy()
+            slide_window_document.text_resource.text = document_text
+            slide_window_documents.append(slide_window_document)
 
         # This ensures that each node is a separate slide
-        slide_splitter = SentenceSplitter(chunk_size=200, chunk_overlap=10)
-        slide_nodes = slide_splitter.get_nodes_from_documents(documents=pdf_documents)
+        slide_splitter = SentenceSplitter(chunk_size=100000, chunk_overlap=0)
+        slide_nodes = slide_splitter.get_nodes_from_documents(documents=slide_window_documents)
             
     if os.path.exists(transcript_directory):
         transcript_documents = SimpleDirectoryReader(transcript_directory).load_data()
